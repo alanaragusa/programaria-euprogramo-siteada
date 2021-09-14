ea3a5d53d2d6d8e31cf22dc74ebba229ce8496ce
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-09-14 01:11:47 +0000

    add js file

---
+++ js/script.js
@@ -0,0 +1,5 @@
+if (document.getElementById("nome") estiver preenchido && document.getElementById("email") estiver preenchido){
+  envie os dados
+}else{
+  peca a usuaria que preencha os campos de nome e email
+}
